f3257241e7eb0b34b7a27336b411cee7e9ab0712
Mac Shu <mac.shu@outlook.com>
2023-08-30 00:12:56 +0800

    fix: update contentful plugin

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,12 +1,19 @@
 import * as contentful from 'contentful'
 
 export default defineNuxtPlugin(() => {
+  const client = contentful.createClient 
+    ? contentful.createClient({
+      space: useRuntimeConfig().public.ctfSpaceId,
+      accessToken: useRuntimeConfig().public.ctfCdaAccessToken
+    })
+    : contentful.default.createClient({
+      space: useRuntimeConfig().public.ctfSpaceId,
+      accessToken: useRuntimeConfig().public.ctfCdaAccessToken
+    })
+
   return {
     provide: {
-      contentfulClient: contentful.createClient({
-        space: useRuntimeConfig().public.ctfSpaceId,
-        accessToken: useRuntimeConfig().public.ctfCdaAccessToken
-      })
+      contentfulClient: client
     }
   }
 })
